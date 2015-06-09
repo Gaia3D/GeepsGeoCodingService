867ac9e04777cad6622ab7bb89f637b38a8c9197
jangbi <jangbi882@gmail.com>
2015-06-09 21:23:02 +0900

    엑셀 저장 추가

--- static/service_page.js
+++ static/service_page.js
@@ -45,6 +45,9 @@ function onClickButton() {
         case "run":
             convertAddr();
             break;
+        case "btnExport":
+            fnExcelReport();
+            break;
         default :
             alert(id);
             break;
@@ -67,7 +70,7 @@ function convertAddr() {
     var crs = $("#crs").val();
 
     // 변환결과 초기화
-    $("#result_table table tbody").html("");
+    $("#result_table tbody").html("");
     if (markerGroup) map.removeLayer(markerGroup);
     markerGroup = L.layerGroup().addTo(map);
 
@@ -107,7 +110,7 @@ function convertAddr() {
             }
 
             if (!data.geojson) {
-                $("#result_table table tbody").append("<tr><td>" + data.q + "</td><td colspan=8>변환 실패</td></tr>");
+                $("#result_table tbody").append("<tr><td>" + data.q + "</td><td colspan=8>변환 실패</td></tr>");
 
                 return;
             }
@@ -120,7 +123,7 @@ function convertAddr() {
             var html = '<tr><td>'+data.q+'</td><td>'+address+'</td><td>'+ x.toFixed(2)+'</td><td>'+y.toFixed(2)+'</td>'
                 +'<td>'+lng.toFixed(5)+'</td><td>'+lat.toFixed(5)+'</td><td>'+data.geojson.properties.service+'</td>'
                 +'<td>'+data.sd+'</td><td>'+data.sim_ratio+'</td></tr>';
-            $("#result_table table tbody").append(html);
+            $("#result_table tbody").append(html);
 
             var marker = L.marker([lat, lng]);
             marker.bindPopup(address);
@@ -136,4 +139,43 @@ function convertAddr() {
             }
         });
     }
+}
+
+
+// 엑셀 파일로 저장
+// http://stackoverflow.com/questions/22317951/export-html-table-data-to-excel-using-javascript-jquery-is-not-working-properl
+function fnExcelReport()
+{
+    var tab_text='<!DOCTYPE html><html><head lang="ko"><meta charset="UTF-8"></head><body>'
+        +"<table border='2px'><tr bgcolor='#87AFC6'>";
+    var textRange; var j=0;
+    tab = document.getElementById('result_table'); // id of table
+
+    for(j = 0 ; j < tab.rows.length ; j++)
+    {
+        tab_text=tab_text+tab.rows[j].innerHTML+"</tr>";
+        //tab_text=tab_text+"</tr>";
+    }
+
+    tab_text=tab_text+"</table>";
+    tab_text= tab_text.replace(/<A[^>]*>|<\/A>/g, "");//remove if u want links in your table
+    tab_text= tab_text.replace(/<img[^>]*>/gi,""); // remove if u want images in your table
+    tab_text= tab_text.replace(/<input[^>]*>|<\/input>/gi, ""); // reomves input params
+    tab_text += "</body></html>";
+
+    var ua = window.navigator.userAgent;
+    var msie = ua.indexOf("MSIE ");
+
+    if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./))      // If Internet Explorer
+    {
+        txtArea1.document.open("txt/html","replace");
+        txtArea1.document.write(tab_text);
+        txtArea1.document.close();
+        txtArea1.focus();
+        sa=txtArea1.document.execCommand("SaveAs",true,"Say Thanks to Sumit.xls");
+    }
+    else                 //other browser not tested on IE 11
+        sa = window.open('data:application/vnd.ms-excel,' + encodeURIComponent(tab_text));
+
+    return (sa);
 }
