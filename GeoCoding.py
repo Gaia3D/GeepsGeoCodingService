0dfba3dcb66898a9473e91c928e85852b5faf62c
jangbi <jangbi882@gmail.com>
2015-06-10 11:24:20 +0900

    주소정제 파일분리, id관련 버그 수정

--- GeoCoding.py
+++ GeoCoding.py
@@ -10,17 +10,21 @@ from pyproj import Proj
 from math import sqrt
 from difflib import SequenceMatcher
 import logging
-import re
+
+# 주소 정제를 위한 함수를 별도 파일로 분리
+from FixAddress import format_address
 
 
 # 로깅 모드 설정
 logging.basicConfig(level=logging.INFO)
+#LOG_FILE_PATH = "D:/www_python/GeoCoding.log"
+LOG_FILE_PATH = "/temp/GeoCoding.log"
 
 # 로깅 형식 지정
 # http://gyus.me/?p=418
 logger = logging.getLogger("failLogger")
 formatter = logging.Formatter('[%(levelname)s] %(asctime)s > %(message)s')
-fileHandler = logging.FileHandler("/temp/GeoCoding.log")
+fileHandler = logging.FileHandler(LOG_FILE_PATH)
 # streamHandler = logging.StreamHandler()
 fileHandler.setFormatter(formatter)
 # streamHandler.setFormatter(formatter)
@@ -142,7 +146,7 @@ def geo_coding():
         output = request.args.get('output', 'json').lower()
         crs = request.args.get('crs', 'epsg:4326').lower()
         # 입력된 id가 있으면 보존되게 수정
-        id = request.args.get('id', None).lower()
+        id = request.args.get('id', None)
 
         # 입력 주소의 정제가 필요
         q = format_address(q)
@@ -445,61 +449,6 @@ def format_res(res, name):
         return "{}:\t({},{}) - {}<br/>".format(name, res['x'], res['y'], res['address'])
     else:
         return "{}:\tError<br/>".format(name)
-
-
-def format_address(org_address):
-    out_address = re.sub(u"\s+", u" ", org_address)
-    out_address = re.sub(u"^한국\s", u"", out_address)
-    out_address = re.sub(u"^서울\s특별시\s", u"서울특별시 ", out_address)
-    out_address = re.sub(u"^서울\s", u"서울특별시 ", out_address)
-    out_address = re.sub(u"^서울시\s", u"서울특별시 ", out_address)
-    out_address = re.sub(u"^인천\s광역시\s", u"인천광역시 ", out_address)
-    out_address = re.sub(u"^인천\s", u"인천광역시 ", out_address)
-    out_address = re.sub(u"^인천시\s", u"인천광역시 ", out_address)
-    out_address = re.sub(u"^대구\s광역시\s", u"대구광역시 ", out_address)
-    out_address = re.sub(u"^대구\s", u"대구광역시 ", out_address)
-    out_address = re.sub(u"^대구시\s", u"대구광역시 ", out_address)
-    out_address = re.sub(u"^울산\s광역시\s", u"울산광역시 ", out_address)
-    out_address = re.sub(u"^울산\s", u"울산광역시 ", out_address)
-    out_address = re.sub(u"^울산시\s", u"울산광역시 ", out_address)
-    out_address = re.sub(u"^부산\s광역시\s", u"부산광역시 ", out_address)
-    out_address = re.sub(u"^부산\s", u"부산광역시 ", out_address)
-    out_address = re.sub(u"^부산시\s", u"부산광역시 ", out_address)
-    out_address = re.sub(u"^광주\s광역시\s", u"광주광역시 ", out_address)
-    out_address = re.sub(u"^광주\s", u"광주광역시 ", out_address)
-    out_address = re.sub(u"^광주시\s", u"광주광역시 ", out_address)
-    out_address = re.sub(u"^대전\s광역시\s", u"대전광역시 ", out_address)
-    out_address = re.sub(u"^대전\s", u"대전광역시 ", out_address)
-    out_address = re.sub(u"^대전시\s", u"대전광역시 ", out_address)
-    out_address = re.sub(u"^경기\s", u"경기도 ", out_address)
-    out_address = re.sub(u"^강원\s", u"강원도 ", out_address)
-    out_address = re.sub(u"^경남\s", u"경상남도 ", out_address)
-    out_address = re.sub(u"^경남도\s", u"경상남도 ", out_address)
-    out_address = re.sub(u"^경북\s", u"경상북도 ", out_address)
-    out_address = re.sub(u"^경북도\s", u"경상북도 ", out_address)
-    out_address = re.sub(u"^제주\s", u"제주특별자치도 ", out_address)
-    out_address = re.sub(u"^제주도\s", u"제주특별자치도 ", out_address)
-    out_address = re.sub(u"^전남\s", u"전라남도 ", out_address)
-    out_address = re.sub(u"^전남도\s", u"전라남도 ", out_address)
-    out_address = re.sub(u"^전북\s", u"전라북도 ", out_address)
-    out_address = re.sub(u"^전북도\s", u"전라북도 ", out_address)
-    out_address = re.sub(u"^충남\s", u"충청남도 ", out_address)
-    out_address = re.sub(u"^충남도\s", u"충청남도 ", out_address)
-    out_address = re.sub(u"^충북\s", u"충청북도 ", out_address)
-    out_address = re.sub(u"^충북도\s", u"충청북도 ", out_address)
-    out_address = re.sub(u"\s산(\d+)", u' 산 \g<1>', out_address)
-    out_address = re.sub(u"\s(\d+가)\s", u'\g<1> ', out_address)
-    out_address = re.sub(u"\s(\d+동)\s", u'\g<1> ', out_address)
-    out_address = re.sub(u"\s(\d+리)\s", u'\g<1> ', out_address)
-    out_address = re.sub(u"\s(\d+)번지\s*(\d+)호", u'\g<1>-\g<2>', out_address)
-    out_address = re.sub(u"(\d+)번지", u'\g<1>', out_address)
-    out_address = re.sub(u"\(.*\)", u'', out_address)  # 새주소 동명 및 건물명 제거
-    out_address = re.sub(u"동(\d+)", u'동 \g<1>', out_address)
-    out_address = re.sub(u"리(\d+)", u'리 \g<1>', out_address)
-    out_address = re.sub(u"가(\d+)", u'가 \g<1>', out_address)
-    out_address = re.sub(u"길(\d+)", u'길 \g<1>', out_address)
-
-    return out_address
 
 
 def query(q, service_name, result):
